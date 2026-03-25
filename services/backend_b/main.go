8e7f8a56cd9c2c7356f780029591b745ee73fa32
fahrisnassief <fahris0310@gmail.com>
2026-03-25 15:18:34 -0600

    Refactored backend_b to show a different service than backend_a

--- services/backend_b/main.go
+++ services/backend_b/main.go
@@ -5,16 +5,16 @@ import (
 	"encoding/json"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+func registerUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	resp := map[string]string {
 		"service": "backend_b",
-		"message": "hello from backend_b",
+		"message": "register user",
 	}
 	_ = json.NewEncoder(w).Encode(resp)
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/register", registerUserHandler)
 	http.ListenAndServe(":8082", nil)
 }
