8b3c3c8126424bc9f584a00bd7c7d9870ced28e3
fahrisnassief <fahris0310@gmail.com>
2026-04-02 22:02:17 -0600

    Added godotenv dependency for .env file reading through os. Built out auth/jwt.go ValidateJWT func. This function will end up being called by the handler to validate jwt before moving onto any proxying.

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.22
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,1 +1,26 @@
-// "How do we work with JWTs?"
+package auth
+
+import (
+	"errors"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	secretKey := os.Getenv("SECRET")
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (any, error) {
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, err
+}
