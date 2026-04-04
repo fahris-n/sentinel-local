d0fdf848c84d852456f60c6b0dfa9acddd3981d3
fahrisnassief <fahris0310@gmail.com>
2026-04-03 22:05:28 -0600

    Refactored handler.go to be single responsibility. Removed JWT validation from inside handler func

--- internal/gateway/handler.go
+++ internal/gateway/handler.go
@@ -1,12 +1,8 @@
 package gateway
 
 import (
-	"log"
 	"net/http"
 	"net/http/httputil"
-
-	"github.com/golang-jwt/jwt/v5/request"
-	"github.com/fahris-n/sentinel-local/internal/auth"
 )
 
 type Handler struct {
@@ -20,23 +16,6 @@ func NewHandler(routeMap map[string]*httputil.ReverseProxy) *Handler {
 }
 
 func (h *Handler) HandleRequest(w http.ResponseWriter, r *http.Request) {
-	// check jwt
-	extractor := &request.BearerExtractor{}
-	tokenStr, err := extractor.ExtractToken(r)
-	if err != nil {
-		log.Println("TokenExtractor error:", err)
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-	claims, err := auth.ValidateJWT(tokenStr)
-	if err != nil {
-		log.Println("JWT error:", err)
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-	// ^^later on we can use something like claims.Role to give user specific tooling/resources
-	log.Println("UserID:", claims.UserID)
-
 	proxy, ok := h.RouteMap[r.URL.Path]
 	if !ok {
 		http.NotFound(w, r)
