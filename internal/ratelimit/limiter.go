d172c27511e956fe7c19121de74354f29b34bcd7
fahrisnassief <fahris0310@gmail.com>
2026-05-07 18:24:32 -0600

    Add rate limiter with Allow method for token bucket enforcement

--- internal/ratelimit/limiter.go
+++ internal/ratelimit/limiter.go
@@ -1,10 +1,33 @@
 package ratelimit
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/redis/go-redis/v9"
 )
 
 type Limiter struct {
 	Client *redis.Client
 	Script *redis.Script
+}
+
+func NewLimiter(client *redis.Client, script *redis.Script) *Limiter {
+	return &Limiter{
+		Client: client,
+		Script: script,
+	}
+}
+
+func (l *Limiter) Allow(userId string, maxTokens int16, refillRate int16) (bool, error) {
+	res, err := l.Script.Run(context.Background(), l.Client, []string{userId}, maxTokens, refillRate).Result()
+	if err != nil {
+		return false, fmt.Errorf("failed to query redis: %w", err)
+	}
+
+	if res.(int64) != 1 {
+		return false, nil
+	}
+
+	return true, nil
 }
